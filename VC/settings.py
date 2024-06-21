7540975df6250c718864f6a83937325caf11c15a
atoms <atomsis01@mail.ru>
2024-06-21 14:55:59 +0300

    Реализация иконки сайта + вебсокета(но не отображает сообщения в чате)

--- VC/settings.py
+++ VC/settings.py
@@ -106,8 +106,12 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_URLTICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
