7d558282056af7e6459fa07140fe0d89d26b0c42
atomsis <75253147+atomsis@users.noreply.github.com>
2024-07-22 11:17:57 +0300

    Update account.urls.py = remove urls login

--- account/urls.py
+++ account/urls.py
@@ -10,12 +10,7 @@ app_name = 'account'
 urlpatterns = [
     # --------------authentication---------------------------------------------------
     path('register/', views.register, name='register'),
-
-    # path('login/', views.user_login, name='login'),
-    # path('login/', views.LoginUser.as_view(), name='login'),
     path('login/', views.login_view, name='login'),
-
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
     path('logout/', views.logout_view, name='logout'),
 
     path('password-change/', views.UserPasswordChange.as_view(), name='password_change'),
