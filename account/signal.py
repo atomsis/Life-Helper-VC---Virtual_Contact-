70bced6ad47c476d8a48ef93982d892d2ffc29c2
atomsis <75253147+atomsis@users.noreply.github.com>
2024-07-22 11:18:56 +0300

    Update account.signal.py = remove signal "create_or_update_profile"(other version)

--- account/signal.py
+++ account/signal.py
@@ -1,16 +1,3 @@
-# from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from .models import Profile
-#
-# @receiver(post_save, sender=User)
-# def create_or_update_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance, email=instance.email)
-#     else:
-#         instance.profile.save()
-
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
