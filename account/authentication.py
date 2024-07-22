f9f9dd2283026c50b7d1eb728aa0f26e1ba9dce0
atomsis <75253147+atomsis@users.noreply.github.com>
2024-07-22 11:23:53 +0300

    Update account.authentication.py = remove user_model

--- account/authentication.py
+++ account/authentication.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.backends import get_user_model
 
 class EmailAuthBackend:
-    # user_model = get_user_model()
     def authenticate(self, request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
@@ -13,7 +12,6 @@ class EmailAuthBackend:
             return None
 
     def get_user(self, user_id):
-        # user_model = get_user_model()
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
