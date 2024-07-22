06296dda496d43a41c8003751bcfe77b0ec3d608
atomsis <75253147+atomsis@users.noreply.github.com>
2024-07-22 11:16:08 +0300

    Update VC.urls.py = remove urls admin

--- VC/urls.py
+++ VC/urls.py
@@ -6,13 +6,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-###------------------------------- Admin ---------------------------------------------------------------
     path('admin/', admin.site.urls),
-    # # URL для входа в административную панель
-    # path('admin/login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='admin_login'),
-    # # URL для выхода из административной панели
-    # path('admin/logout/', auth_views.LogoutView.as_view(next_page='/'), name='admin_logout'),
-###-----------------------------------------------------------------------------------------------------
     path('', include('account.urls')),
     path('weather/',include('weather_api.urls')),
     path('money_tracker/',include('money_tracker.urls',namespace='money_tracker')),
